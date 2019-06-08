c28bfe2f2131837dffb6a1b4c313bbe6543ea56b
previdus <ricky.rungta@gmail.com>
2019-06-08 17:49:47 +0530

    Initial project checkin

---
+++ src/main/java/com/previdus/educoaster/EducoasterApplication.java
@@ -0,0 +1,13 @@
+package com.previdus.educoaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducoasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducoasterApplication.class, args);
+	}
+
+}
